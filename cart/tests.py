38f2b2f3b27d995a4b15154cc66fb1978c89ffe3
Daniel Halliday <daniel.j.halliday@gmail.com>
2017-08-25 11:24:44 +0100

    deploy to Heroku

--- cart/tests.py
+++ cart/tests.py
@@ -1,3 +1,9 @@
 from django.test import TestCase
 
-# Create your tests here.
+from .views import user_cart
+from django.core.urlresolvers import resolve
+ 
+class CartTest(TestCase):
+    def test_cart_resolves(self):
+        cart_page = resolve('/cart/')
+        self.assertEqual(cart_page.func, user_cart)
